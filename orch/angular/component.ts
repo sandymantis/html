8301e4b84d18617183a2ef3fa56445fe66ee852b
sandymantis <146299993+sandymantis@users.noreply.github.com>
2024-09-11 08:20:54 -0500

    Update component.ts

--- orch/angular/component.ts
+++ orch/angular/component.ts
@@ -18,3 +18,38 @@ export class MyComplexComponent implements OnInit {
     this.counter++;
   }
 }
+
+
+-----------
+
+  import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-complex',
+  templateUrl: './app-complex.component.html',
+  styleUrls: ['./app-complex.component.css']
+})
+export class AppComplexComponent implements OnInit {
+  apiData: string[] = []; // Data fetched from the API
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    // This API call is made during SSR to populate the data on the server side
+    this.fetchDataFromAPI();
+  }
+
+  fetchDataFromAPI(): void {
+    // Example API call to a publicly available API
+    this.http.get<string[]>('https://jsonplaceholder.typicode.com/posts?_limit=5')
+      .subscribe(data => {
+        this.apiData = data.map(post => post['title']); // Map the API data to the desired format
+      });
+  }
+
+  refreshData(): void {
+    // This method is called on the client side during CSR
+    this.fetchDataFromAPI();
+  }
+}
